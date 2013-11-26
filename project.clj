f8218e30f86e1fce1f0b0b31c34593e726204f91
Daniel <D.Kiertscher@gmx.de>
2013-11-26 14:47:46 +0100

    hebe Ausgangszustand auf
    vor Durchstich des XML-Parsers

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject sigetdata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "parsing wikipedia dump compressed xml files"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [me.raynes/fs "1.4.4"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
+
+; Set-Proxy-Envvar-FHB -off
+; Set-Proxy-Envvar-FHB
--- test/sigetdata/core_test.clj
+++ test/sigetdata/core_test.clj
@@ -1,7 +1,31 @@
+; start unittest automation in a shell with
+; lein repl
+; (use 'midje.repl) (autotest)
+
 (ns sigetdata.core-test
   (:use clojure.test
-        sigetdata.core))
+        midje.sweet
+        sigetdata.core)
+(:require [clojure.java.io :as io]
+          [me.raynes.fs :as fs]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "fs get file name"
+       (fact (fs/name wikifile3) => "dewiki-20131008-stub-articlest")
+       (fact (fs/mod-time wikifile3) => 1383148406618))
+
+(deftest test-nzb->map
+  (let [input (io/resource "example.nzb")]
+    (is (= {:meta {:title "Your File!"
+                   :tag "Example"}
+            :files [{:poster "Joe Bloggs <bloggs@nowhere.example>"
+                     :date 1071674882
+                     :subject "Here's your file!  abc-mr2a.r01 (1/2)"
+                     :groups ["alt.binaries.newzbin"
+                              "alt.binaries.mojo"]
+                     :segments [{:bytes 102394
+                                 :number 1
+                                 :id "123456789abcdef@news.newzbin.com"}
+                                {:bytes 4501
+                                 :number 2
+                                 :id "987654321fedbca@news.newzbin.com"}]}]}
+           (nzb->map input)))))
