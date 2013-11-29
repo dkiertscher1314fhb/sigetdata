2aad84a8891925aaeac577332c480b5776375740
Daniel <D.Kiertscher@gmx.de>
2013-11-29 19:04:16 +0100

    lege Beispielcode fuer Threading bereit

--- interactive.clj
+++ interactive.clj
@@ -17,3 +17,14 @@
 ; Maven proxy configuration:
 ; Set-Proxy-Envvar-FHB
 ; Set-Proxy-Envvar-FHB -off
+
+; multithreading
+(def long-calculation (future (apply + (range 1e8))))
+@long-calculation
+(println "done")
+
+(def bg (future (Thread/sleep 8000) (println "done1")))
+(def bg (future (Thread/sleep 4000) (println "done2")))
+(def bg (future (Thread/sleep 9000) (println "done3")))
+(def bg (future (Thread/sleep 2000) (println "done4")))
+
