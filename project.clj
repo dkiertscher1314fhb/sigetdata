e1662042d564d371b3299fab5322ca84e4695e40
Daniel <D.Kiertscher@gmx.de>
2013-12-25 10:28:19 +0100

    getting exploration of neocons from Jonas

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.clojure/data.xml "0.0.7"]
                  [me.raynes/fs "1.4.4"]
                  [mysql/mysql-connector-java "5.1.27"]
-                 [org.clojure/java.jdbc "0.3.0-beta2"]]
+                 [org.clojure/java.jdbc "0.3.0-beta2"]
+                 [clojurewerkz/neocons "2.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]
                    :plugins [[lein-midje "3.0.0"]]}})
 
